edc4682376101221d7a5dde17887473dac8ef8a5
sergeygurylev <ohg@mail.ru>
2023-11-02 22:34:03 +0300

    2

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -1,8 +1,59 @@
 import Cart from '../service/Cart';
+import Book from "../domain/Book";
+import MusicAlbum from "../domain/MusicAlbum";
+import Movie from "../domain/Movie";
+
+const book = new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225);
+const album = new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900);
+const movie = new Movie(
+    1009,
+    'The Avengers',
+    0,
+    2012,
+    'USA',
+    'Avengers Assemble!',
+    ['fantastic', 'action', 'adventures'],
+    137);
 
 test('new card should be empty', () => {
   const cart = new Cart();
+  expect(cart.items.length).toBe(0);
+});
 
-  // expect(cart.items.length).toBe(0);
-  expect(1).toBe(1);
+test('add products', () => {
+  const cart = new Cart();
+  cart.add(book);
+  cart.add(album);
+  cart.add(movie);
+
+  expect(cart.items.length).toBe(3);
+});
+
+test('delete products', () => {
+  const cart = new Cart();
+  cart.add(book);
+  cart.add(album);
+  cart.add(movie);
+
+  cart.delete(1008);
+
+  expect(cart.items.length).toBe(2);
+});
+
+test('getAmount without discount', () => {
+  const cart = new Cart();
+  cart.add(book);
+  cart.add(album);
+  cart.add(movie);
+
+  expect(cart.getAmount()).toBe(2900);
+});
+
+test('getAmount with discount', () => {
+  const cart = new Cart();
+  cart.add(book);
+  cart.add(album);
+  cart.add(movie);
+
+  expect(cart.getAmount(10)).toBe(2900 - 290);
 });
